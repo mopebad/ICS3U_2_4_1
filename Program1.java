04f9170157797c4aaa50dca0811b46f4c5532ffd
mopebad <joshmopebad@gmail.com>
2023-10-16 09:43:08 -0400

    .

--- Program1.java
+++ Program1.java
@@ -1,12 +1,24 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description: 
+  * @author: Joshua
   */
   
   public void run() {
     
-    
+    // Initialize variables
+    int intN = readInt("Enter N:"); 
+    int intSum = 0; //set the accumulator variable
+
+    // accumulate sum
+    for (int i = 1; i <= intN; i++){
+      // update our accumulator variable
+      intSum = intSum + i;
+    }
+
+    //output result
+    System.out.println(intSum);
+
   }
 }
--- Program2.java
+++ Program2.java
@@ -1,11 +1,17 @@
 class Program2 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description: Converts Km to Miles from 10km to 100km in incraments of 10km.
+  * @author: Joshua
   */
   
   public void run() {
-   
+
+    double dblMiles;
+    
+    for (int intKm = 10; intKm <= 100; intKm += 10){
+      dblMiles = intKm * 0.621371;
+      System.out.println(intKm + "km --> " + dblMiles + " mi");
+    }
   }
 }
